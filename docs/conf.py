14ce5c04dbbc8db9371957c8651b678af6c45b37
mdpopovici <mdpopovici@gmail.com>
2016-02-07 19:08:14 +0200

    _

--- docs/conf.py
+++ docs/conf.py
@@ -128,11 +128,6 @@ html_short_title = 'Net-Instant'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
-        ],
-    }
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
